ff80a4670fc44be25bc203a5c43f3570b07d29c2
Alejandro Benjumea <alejandro.benjumea@superworldapp.com>
2022-05-28 14:05:38 -0500

    refactor(ci): fix and format config files

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,18 +1,17 @@
 const { TsconfigPathsPlugin } = require('tsconfig-paths-webpack-plugin');
 
 module.exports = {
-  stories: [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.(ts|tsx)"
-  ],
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.(ts|tsx)'],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
   ],
-  framework: "@storybook/react",
+  framework: '@storybook/react',
   webpackFinal: async (config) => {
-    config.resolve.plugins = [new TsconfigPathsPlugin({ extensions: config.resolve.extensions })]
-    return config
-}
-}
+    config.resolve.plugins = [
+      new TsconfigPathsPlugin({ extensions: config.resolve.extensions }),
+    ];
+    return config;
+  },
+};
